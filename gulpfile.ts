92b2bef4081d5a844bac91c6fcfc9b5bd35d30da
William Heesen <vindicayte@gmail.com>
2017-04-28 10:27:06 -0400

    Separate contents of node package into src folder.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,14 @@
 import gulp = require("gulp");
 import rename = require("gulp-rename")
+import structify from 'gulp-structify/main'
 import { Gulpclass, Task } from 'gulpclass'
-import * as structify from './src/main';
 
 @Gulpclass()
 export class Gulpfile {
     @Task()
     structify() {
         return gulp.src("./examples/**/*.template.ts")
-            .pipe(structify.run())
+            .pipe(structify())
             // Remove ".template" from filename
             .pipe(rename(p => {  
                 let base = p.basename;
