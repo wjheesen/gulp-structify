814572ca49736542009c6dfed4cb9f242bf89ba5
William Heesen <vindicayte@gmail.com>
2017-05-03 15:23:33 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 # Clean lib folder and self reference folder
-Remove-Item ./lib/*, ./node_modules/gulp-structify/*
+Remove-Item ./lib, ./node_modules/gulp-structify -Recurse -ErrorAction Ignore
 # Transpile src files (with "outDir" set to lib folder)
 tsc --project ./src/tsconfig.json
 # Copy package files to lib folder
 Copy-Item -Path package.json, README.md, LICENSE -Destination ./lib
 # Copy lib folder to node_modules (for self reference)
-Copy-Item -Path ./lib/* -Destination ./node_modules/gulp-structify
+Copy-Item -Path ./lib -Destination ./node_modules/gulp-structify -Recurse
 
