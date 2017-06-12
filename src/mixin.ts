b5bacceac4cefbb1a90dc04f98c20529633ed57c
William Heesen <vindicayte@gmail.com>
2017-06-12 13:06:39 -0400

    Fix typo in applyMixins function.

--- src/mixin.ts
+++ src/mixin.ts
@@ -1,10 +1,18 @@
 let ctor = "constructor";
 
-export function applyMixins(derivedCtor: any, ...baseCtors: any[]) {
+export interface Constructor {
+    prototype: Prototype;
+}
+
+export interface Prototype extends Object {
+    [key: string]: any;
+}
+
+export function applyMixins(derivedCtor: Constructor, ...baseCtors: Constructor[]) {
     for(let baseCtor of baseCtors){
         for(let name of Object.getOwnPropertyNames(baseCtor.prototype)){
             if(name !== ctor){
-                derivedCtor.protoype[name] = baseCtor.prototype[name];
+                derivedCtor.prototype[name] = baseCtor.prototype[name];
             }
         }
     }
